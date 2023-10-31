934b020cff5fcd3c8e243553f1e385c3ded15a55
rivitch <rivitch@rambler.ru>
2023-10-31 08:37:19 +0400

    1

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,10 @@
 # (22:30) создаем файл wsgi.py
 
-from lec1_1 import app
-from lec1_2 import app
-from lec1_3 import app
-from lec1_4 import app
-from lec1_5 import app
-from lec1_6 import app
-from lec1_7 import app
-from lec1_8 import app
+from Task1_9 import app
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-# flask run --debug # не работает
-# РЕШЕНИЕ - 'python -m flask run --debug'
