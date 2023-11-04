20425e4fecb7ddb403a51b58192dcfcd6d6e1984
rivitch <rivitch@rambler.ru>
2023-11-04 04:23:53 +0400

    4:23

---
+++ app_01.py
@@ -0,0 +1,21 @@
+# Экранирование пользовательских данных
+
+from flask import Flask #, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Введи путь к файлу в адресной строке'
+
+
+@app.route('/<path:file>/')
+def get_file(file):
+    print(file)
+    # return f'Ваш файл находится в: {file}!'  # без экранирования
+    return f'Ваш файл находится в: {escape(file)}!'  # с экранированием
+
+if __name__ == "__main__":
+    app.run(debug=True)
