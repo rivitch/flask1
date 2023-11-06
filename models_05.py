ab0bc92ead8ecc1b791a68f576ba4a58492bfc1b
rivitch <rivitch@rambler.ru>
2023-11-06 20:28:32 +0400

    1a

--- models_05.py
+++ models_05.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 db = SQLAlchemy()
--- wsgi1.py
+++ wsgi1.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from Lec3_4 import app
 
 if __name__ == "__main__":
